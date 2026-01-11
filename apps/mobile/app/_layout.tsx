7af3638ff054a74fe9b80c4baacc9ddf7c0d7533
kekubhai <anirbanghosh060@gmail.com>
2026-01-11 23:37:10 +0530

    done

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,11 +1,12 @@
 import { tokenCache } from '@/utils/cache'
 import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo'
 import { Slot, Stack } from 'expo-router'
+import { LogBox } from 'react-native'
 const clerkPublishableKey=process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY|| ''
 if(!clerkPublishableKey){
   throw new Error('Clerk publishable key is not defined')
 }
-
+LogBox.ignoreAllLogs
 const InitialLayout=()=>{
   return ( 
     <Stack>
