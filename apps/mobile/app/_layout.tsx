245f9e559eb783f77681f51da4a21108e4e73455
kekubhai <anirbanghosh060@gmail.com>
2026-01-11 22:14:23 +0530

    setup done

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,7 +1,24 @@
-import { Stack } from 'expo-router';
+import { tokenCache } from '@/utils/cache'
+import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo'
+import { Slot, Stack } from 'expo-router'
+const clerkPublishableKey=process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY|| ''
+if(!clerkPublishableKey){
+  throw new Error('Clerk publishable key is not defined')
+}
 
-export default function Layout() {
-  return <Stack  
-    screenOptions={{ headerShown: false }}
-  />;
+const InitialLayout=()=>{
+  return ( 
+    <Stack>
+      <Stack.Screen name='index' />
+    </Stack>
+  )
+}
+export default function RootLayout() {
+  return (
+    <ClerkProvider publishableKey={clerkPublishableKey} tokenCache={tokenCache}>
+      <ClerkLoaded>
+      <Slot  />
+      </ClerkLoaded>
+    </ClerkProvider>
+  )
 }
